b368cdcb7395f66957e8a7ed47fa2a1845db1d25
susmitatier5 <susmita.biswas@tier5.in>
2024-05-18 01:04:03 +0530

    FRMAP-1902 sync issue fixation (#357)

--- src/extensionScript/getFriendLength.js
+++ src/extensionScript/getFriendLength.js
@@ -1,15 +1,17 @@
 import selectors from "./selector";
 
 const timer = setInterval(() => {
-    const noOfMyFriends = document.querySelectorAll(selectors.friend_count)
-    // const noOfMyFriends = document.querySelectorAll('h2[dir="auto"]')
-    // console.log("noOfMyFriends : ", noOfMyFriends, noOfMyFriends.length)
-    if(noOfMyFriends.length > 0){
+    const divv = document.querySelector(selectors.all_friends_div)
+    if(divv){
         clearInterval(timer)
-        let friendLength =  noOfMyFriends[noOfMyFriends.length - 1].innerText.replace(/[^\d]/g, "");
-        
-        // console.log("FR length", noOfMyFriends, noOfMyFriends[noOfMyFriends.length - 1].innerText.replace(/[^\d]/g, ""));
-        
+        let friend_h2 = divv.querySelector(selectors.friend_count);
+        if(!friend_h2){
+            const seperator = document.querySelector(selectors.seperator);
+            const parent_div = seperator.parentElement
+            friend_h2 = parent_div.children[2].querySelector(selectors.friend_count)
+        }
+        let friendLength = friend_h2.textContent.replace(/[^\d]/g, "")
+        console.log(friendLength)
         chrome.runtime.sendMessage({action : "friendCount", friendLength: friendLength});
     }
 }, 1000)
