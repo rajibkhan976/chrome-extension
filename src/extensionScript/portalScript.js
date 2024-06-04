0ee47dda3d496e7c1f09bd2b94a83b23027a2259
rajibtier5 <139124128+rajibtier5@users.noreply.github.com>
2024-06-04 13:21:52 +0800

    sending message from extension to portal is completed (#375)

--- src/extensionScript/portalScript.js
+++ src/extensionScript/portalScript.js
@@ -2,35 +2,32 @@
  * Through this script extension will communicate
  * with web portal using localstorage, cookie, etc.
  */
-import helper from "./helper";
+import helper from "./helper"
+console.log("****************************Portal content script is injected******************************************")
 
-console.log(
-	"****************************Portal content script is injected******************************************"
-);
+chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
+    console.log("ACTION", request)
+    if (!request.hasOwnProperty("content")) return false
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	console.log("ACTION", request);
-	if (!request.hasOwnProperty("content")) return false;
+    // Storage type
+    let type = request.type === "cookie" ? "cookie" : "localstorage"
 
-	// Storage type
-	let type = request.type === "cookie" ? "cookie" : "localstorage";
-
-	if (request.content) {
-		if (type === "cookie") {
-			helper.setCookie(request.action, request.content);
-		} else if (request.type === "alert") {
-			window.alert(request.content);
-		} else if (request.type && request.type === "postmessage") {
-			window.postMessage(request.content, process.env.REACT_APP_APP_URL);
-		} else {
-			localStorage.setItem(request.action, request.content);
-		}
-	} else {
-		if (type === "cookie") {
-			helper.deleteCookie(request.action);
-		} else {
-			localStorage.removeItem(request.action);
-		}
-	}
-	return true;
+    if (request.content) {
+        if (type === "cookie") {
+            helper.setCookie(request.action, request.content);
+        } else if(request.type === "alert"){
+            window.alert(request.content);
+        } else if (request.type && request.type === "postmessage") {
+            window.postMessage(request.content, process.env.REACT_APP_APP_URL);
+        } else {
+            localStorage.setItem(request.action, request.content);
+        }
+    } else {
+        if (type === "cookie") {
+            helper.deleteCookie(request.action)
+        } else {
+            localStorage.removeItem(request.action)
+        }
+    }
+    return true;
 });
